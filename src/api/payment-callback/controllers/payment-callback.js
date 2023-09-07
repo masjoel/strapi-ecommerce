1276999240efe8d9d6296f3196f287361b4c4e25
Masjoel <masjoel@gmail.com>
2023-09-07 10:10:40 +0700

    update Payment Callback

--- src/api/payment-callback/controllers/payment-callback.js
+++ src/api/payment-callback/controllers/payment-callback.js
@@ -8,19 +8,26 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::payment-callback.payment-callback', ({ strapi }) => ({
     async create(ctx){
-        let request = ctx.request.body
-        console.log('Request: ', request)
-        let order = await strapi.service('api::order.order').findOne(request.order_id)
-        
-        let params = {}
-        if (request.transaction_status=='settlement'){
-            params = {'data':{"statusOrder": 'purchased'}}
-        }else{
-            params = {'data':{"statusOrder": 'cancel'}}
-        }
-        let updateOrder = await strapi.service('api::order.order').update(request.order_id, params)
-        console.log('update : ', updateOrder)
+        let request = ctx.request.body;
+        console.log('request: ', request);
 
-        return {'data':updateOrder}
+        let order = await strapi.service('api::order.order').findOne(request.order_id);
+        let inputData = { 'data': { 'history': request } };
+
+        const result = await strapi.service('api::payment-callback.payment-callback').create(inputData);
+
+        let params = {}
+
+        if (request.transaction_status == 'settlement') {
+            params = { 'data': { 'statusOrder': 'purchased' } }
+        } else {
+            params = { 'data': { 'statusOrder': 'cancel' } }
+        }
+
+        let updateOrder = await strapi.service('api::order.order').update(request.order_id, params);
+
+        console.log('update data: ', updateOrder);
+
+        return { 'data': updateOrder }
     }
 }));
